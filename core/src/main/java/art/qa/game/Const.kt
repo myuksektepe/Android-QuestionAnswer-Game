5b78ff5cb58bd5d1b8a2aaaad12524d6f7f8235e
myuksektepe <yuksektepemurat@gmail.com>
2024-01-13 23:46:33 +0300

    [DEV][murat.yuksektepe][Strings, Digits and Const files edited]

---
+++ core/src/main/java/art/qa/game/Const.kt
@@ -0,0 +1,7 @@
+package art.qa.game
+
+const val DOT = '.'
+const val THREE_DOT = "..."
+const val SPACE = " "
+const val SHOW_MORE_EN = "$THREE_DOT${SPACE}Show More"
+const val SHOW_LESS_EN = "${SPACE}Show Less"
---
+++ core/src/main/java/art/qa/game/Digits.kt
@@ -0,0 +1,9 @@
+package art.qa.game
+
+const val ZERO = 0
+const val ONE = 1
+const val TWO = 2
+const val THREE = 3
+const val FOUR = 4
+const val FIVE = 5
+const val TEN = 10
